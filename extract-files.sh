089841ea0117ab88076ce01610d699eacf61eded
Danila Suslov <d@suslovd.ru>
2024-01-22 01:29:24 +0000

    [WIP] Use Linaro BSP
    Change-Id: I6fd3b3c93b2f03a837c9ec0aa79adbc7e99250ec

--- extract-files.sh
+++ extract-files.sh
@@ -11,7 +11,7 @@ function blob_fixup() {
         vendor/lib*/libsec-ril*.so)
             "${PATCHELF}" --replace-needed libril.so libril-samsung.so "${2}"
             ;;
-        vendor/lib*/libexynosdisplay.so|vendor/lib*/sensors.*.so)
+        vendor/lib*/sensors.*.so)
             "${PATCHELF}" --replace-needed libutils.so libutils-v32.so "${2}"
             sed -i 's/_ZN7android6Thread3runEPKcim/_ZN7utils326Thread3runEPKcim/g' "${2}"
             ;;
